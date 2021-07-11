b5cd29acd0426c2efa54afdd6c6334636dc807cb
shanole <73411129+shanole@users.noreply.github.com>
2021-07-11 16:03:05 -0700

    working CRUD for images
    Co-Authored-by: Garrett Brown <garrettpaulbrown@gmail.com>
    Co-Authored-by: Salim Mayan <mailsalim@gmail.com>

--- ScribbleBoard/Controllers/ImagesController.cs
+++ ScribbleBoard/Controllers/ImagesController.cs
@@ -3,35 +3,60 @@ using Microsoft.AspNetCore.Mvc;
 using ScribbleBoard.Models;
 using System.Collections.Generic;
 using System.Linq;
+using System.Diagnostics;
 using System;
+using System.Threading.Tasks;
 
 namespace ScribbleBoard.Controllers
 {
-    public class ImagesController : Controller
+  public class ImagesController : Controller
+  {
+    public ActionResult Index()
     {
-        public ActionResult Index()
-        {
-            var allImages = Image.GetAll();
-            return View(allImages);
-        }
-        public IActionResult Create()
-        {
-            return View();
-        }
-        [HttpPost]
-        public IActionResult Create(Image image)
-        {
-            // var userId = this.User.FindFirst(ClaimTypes.NameIdentifier)?.Value;
-            // var currentUser = await _userManager.FindByIdAsync(userId);
-            // image.User = currentUser;
-            // image.UserId = userId;
-            Image.Post(image);
-            return RedirectToAction("Index");
-        }
-        public IActionResult Details(int id)
-        {
-            var image = Image.GetDetails(id);
-            return View(image);
-        }
+      var allImages = Image.GetAll();
+      return View(allImages);
     }
+    public IActionResult Create()
+    {
+      return View();
+    }
+    [HttpPost]
+    public IActionResult Create(Image image)
+    {
+      // var userId = this.User.FindFirst(ClaimTypes.NameIdentifier)?.Value;
+      // var currentUser = await _userManager.FindByIdAsync(userId);
+      // image.User = currentUser;
+      // image.UserId = userId;
+      Image.Post(image);
+      return RedirectToAction("Index");
+    }
+    public IActionResult Details(int id)
+    {
+      var image = Image.GetDetails(id);
+      return View(image);
+    }
+    public IActionResult Delete(int id)
+    {
+      var image = Image.GetDetails(id);
+      return View(image);
+    }
+    [HttpPost, ActionName("Delete")]
+    public IActionResult DeleteConfirmed(int id)
+    {
+      Image.Delete(id);
+      return RedirectToAction("Index");
+    }
+    public IActionResult Edit(int id)
+    {
+      var image = Image.GetDetails(id);
+      return View(image);
+    }
+    [HttpPost]
+    public IActionResult Details(int id, Image image)
+    {
+      image.ImageId = id;
+      Image.Put(image);
+      return RedirectToAction("Details", id);
+    }
+  }
 }
